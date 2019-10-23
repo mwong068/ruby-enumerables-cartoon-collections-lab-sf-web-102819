a21e3fe02a7c50eda7824d953d138f8ceacfb78b
Megan Wong <mwong068@gmail.com>
2019-10-23 05:25:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  array.each do |cheeses|
   if array.include(cheese_types)
-    return 
+    
 end

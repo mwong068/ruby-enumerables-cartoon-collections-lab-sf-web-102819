efd00108d9a3f788844634839ad97381752aefa6
Megan Wong <mwong068@gmail.com>
2019-10-23 05:20:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,12 @@ def summon_captain_planet(array)
   array.map {|word| "#{word.capitalize()}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each do |call|
+    if call.length < 4
+      return false
+    end
+  true
 end
 
 def find_the_cheese# code an argument here
